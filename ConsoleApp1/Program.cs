eb62446b9549ed37fdd6e32f168d2767d0a51f6a
moraghan <peter.moraghan"me.com>
2020-09-30 00:36:13 -0700

    Refactored

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,16 +9,18 @@ namespace ConsoleApp1
     {
         static void Main()
         {
-            List<Datafile> sourceSnapshot = TakeInitialDirSnapshot(@"/Users/moraghan/docker");
-            foreach (Datafile fFile in sourceSnapshot)
-            {
-                Console.WriteLine("{0}: {1}: {2}: {3}: {4}: {5}", fFile.fileName, fFile.lastAccessTime, fFile.size,
-                    fFile.extension, fFile.directory, fFile.status);
-            }
+            List<Datafile> sourceSnapshot = TakeFileSnapshot(@"/Users/moraghan/docker");
             
+            List<Datafile> filesProcessed = CopySourceFilesToTarget(sourceSnapshot);
+
+            foreach (Datafile dFile in filesProcessed)
+            {
+                Console.WriteLine("{0}: {1}: {2}: {3}: {4}: {5}", dFile.fileName, dFile.lastAccessTime, dFile.size,
+                    dFile.extension, dFile.directory, dFile.status);
+            }
         }
 
-        static List<Datafile> TakeInitialDirSnapshot(string sourceDirName)
+        static List<Datafile> TakeFileSnapshot(string sourceDirName)
         {
             List<string> validExtensions = new List<string>() {".txt", ".bak", ".sql"};
             var minModifiedDate = new DateTime(2019,12,18);
@@ -48,6 +50,24 @@ namespace ConsoleApp1
 
             return Datafiles;
             
+        }
+
+        static List<Datafile>  CopySourceFilesToTarget(List<Datafile> DataFilesToBeCopied)
+        {
+            foreach (Datafile dFile in DataFilesToBeCopied)
+            {
+                var fFile = new FileInfo(dFile.directory + '/' + dFile.fileName);
+                // If current file size and last access time same as snapshot then we know file has not changed
+                // and so can be copied to destination
+                if ((fFile.Length == dFile.size) && (fFile.LastAccessTime == dFile.lastAccessTime))
+                {
+                    Console.WriteLine("{0}: {1}: {2}: {3}: {4}: {5}", dFile.fileName, dFile.lastAccessTime, dFile.size,
+                        dFile.extension, dFile.directory, dFile.status);
+                    dFile.status = "Copied";
+                }
+            }
+
+            return DataFilesToBeCopied;
         }
         
     }
